a8181d47a891d4d32b131525e15b45a260a2bce8
jagadeeshKadal <jagadeeshKadali69@gmail.com>
2026-02-04 17:07:03 +0530

    gfdsdfdsdfds

--- csp-backend/src/config/firebase.ts
+++ csp-backend/src/config/firebase.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import * as fs from "fs";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
+// 1
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
